b95705c9fe8c3d0cbf127f320193be8da26d0379
michal <michal.reifer@gmail.com>
2021-05-24 19:46:17 +0300

    added a fetch to useCategories to get the unique categories from the data

--- src/Home.js
+++ src/Home.js
@@ -4,16 +4,19 @@ import useCategories from "./useCategories";
 const Home = () => {
 
     const { categories, allProducts, allLoadings, allErrors } = useCategories();
+    // console.log(allErrors);
 
     return (
         <div className="home">
             { categories.map(function(category, i){
+                // console.log(category, allErrors[i], allLoadings[i], allProducts[i]);
                return( 
                 allErrors[i] && <div>{ allErrors[i] }</div>,
                 allLoadings[i] && <div>Loading...</div>,    
-                allProducts[i] && <ProductPreview products={allProducts[i]} key={category} title={`${category} Cakes`}/>)
+                allProducts[i] && <ProductPreview products={allProducts[i]} key={category} title={category}/>)
                 })
             }
+            
         </div>
     );
 }
--- src/useCategories.js
+++ src/useCategories.js
@@ -1,14 +1,40 @@
+import { useEffect, useState } from "react";
 import useFetch from "./useFetch";
 
 const useCategories = () => {
 
-    const categories = ["Classic", "Crazy", "Chocolate", "Cookies", "Cream"];
+    const productsUrl = 'http://localhost:8000/products';
+    const [categories, setCategories] = useState([]);
 
-    const { data: products0, isLoading0, error0 } = useFetch('http://localhost:8000/products?category='+categories[0]+' Cakes');
-    const { data: products1, isLoading1, error1 } = useFetch('http://localhost:8000/products?category='+categories[1]+' Cakes');
-    const { data: products2, isLoading2, error2 } = useFetch('http://localhost:8000/products?category='+categories[2]+' Cakes');
-    const { data: products3, isLoading3, error3 } = useFetch('http://localhost:8000/products?category='+categories[3]+' Cakes');
-    const { data: products4, isLoading4, error4 } = useFetch('http://localhost:8000/products?category='+categories[4]+' Cakes');
+    const [data, setData] = useState(null);
+    useEffect(() => {     
+        fetch(productsUrl)  
+        .then(res => res.json())   
+        .then(data => {           
+            setData(data);
+            // console.log(data);
+            return data;})
+        .then(data=>{
+            setCategories([...new Set(data.map(it=>it.category))]);
+            // console.log(categories)
+            })
+    }, [productsUrl]);              
+
+    // const allProducts = [];
+    // const allLoadings = [];
+    // const allErrors = [];
+    // categories.map((category) => {
+    //     const { data: products, isLoading, error } = useFetch(productsUrl+'?category='+category);
+    //     allProducts.push(products);
+    //     allLoadings.push(isLoading);
+    //     allErrors.push(error);
+    // }) 
+
+    const { data: products0, isLoading: isLoading0, error: error0 } = useFetch(productsUrl+'?category='+categories[0]);
+    const { data: products1, isLoading: isLoading1, error: error1 } = useFetch(productsUrl+'?category='+categories[1]);
+    const { data: products2, isLoading: isLoading2, error: error2 } = useFetch(productsUrl+'?category='+categories[2]);
+    const { data: products3, isLoading: isLoading3, error: error3 } = useFetch(productsUrl+'?category='+categories[3]);
+    const { data: products4, isLoading: isLoading4, error: error4 } = useFetch(productsUrl+'?category='+categories[4]);
     
     const allProducts = [products0, products1, products2, products3, products4];
     const allLoadings = [isLoading0, isLoading1, isLoading2, isLoading3, isLoading4];
@@ -16,5 +42,5 @@ const useCategories = () => {
 
     return { categories, allProducts, allLoadings, allErrors };
 }
- 
+
 export default useCategories;
