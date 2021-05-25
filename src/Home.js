ca5211d11cf7676165fa957533204b4beaffb62b
michal <michal.reifer@gmail.com>
2021-05-25 14:36:48 +0300

    move all of useCategories code to Home component since its much cleaner and shorter now, and deleted useCategories hook.

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,19 @@
 import ProductPreview from "./ProductPreview";
-import useCategories from "./useCategories";
+import useFetch from "./useFetch";
 
 const Home = () => {
-
-    const { categories, allProducts, isLoading, error } = useCategories();
+    
+    const productsUrl = 'http://localhost:8000/products';
+    let categories = [];
+    const productsByCategory = [];
+    const { data: products, isLoading, error } = useFetch(productsUrl);
+    
+    if (products){
+        categories = [...new Set(products.map(product=>product.category))];
+        categories.map(category=>{
+            return productsByCategory.push(products.filter(product=>product.category===category));
+        })
+    }
 
     return (
         <div className="home">
@@ -11,10 +21,9 @@ const Home = () => {
             { isLoading && <div>Loading...</div>}    
             { categories.map(function(category, i){
                return( 
-                allProducts[i] && <ProductPreview products={allProducts[i]} key={category} title={category}/>)
-                })
+                productsByCategory[i] && <ProductPreview products={productsByCategory[i]} key={i} title={category}/>
+                )})
             }
-            
         </div>
     );
 }
