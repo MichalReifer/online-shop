d6dd5ce1b415ce43a3449d577a44006f02ac4c75
michal <michal.reifer@gmail.com>
2022-08-28 17:48:08 +0300

    Remove Redux Saga Files and Installation

--- src/redux/sagas/requests/users.js
+++
@@ -1,6 +0,0 @@
-
-export function requestGetUsers() {
-    return fetch('http://localhost:8081/users')
-            .then(response=>response.json())
-  }
-  
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,14 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import {cakesReducer, cakeCategoriesReducer} from './slices/cakesSlice'
 import  usersReducer from './slices/usersSlice'
-// redux saga:
-import createSagaMiddleware from "redux-saga";
-import { watcherSaga } from "./sagas/rootSaga";
-
 
 const reducer = combineReducers({
     cakes: cakesReducer,
     cakeCategories: cakeCategoriesReducer,
     users: usersReducer
   });
-
-const sagaMiddleware = createSagaMiddleware();
   
 export const store = configureStore({
   reducer,
-  middleware: (defaultMiddleware) => defaultMiddleware().concat(sagaMiddleware)
 })
 
-sagaMiddleware.run(watcherSaga);
