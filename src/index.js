803cb445c77d1213f1c80faefcc27f40d6ebf8b7
michal <michal.reifer@gmail.com>
2022-09-29 13:45:00 +0300

    CSS - Rearrange Files
    fix indentation and separate styles to more files
    and also add container-width variable

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import './styles/productPreview.css';
 import './styles/productPage.css'
 import './styles/userPage.css'
 import './styles/search.css'
+import './styles/userOrders.css'
+import './styles/sweetAlert.css'
+import './styles/scrollBar.css'
 import App from './App';
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
